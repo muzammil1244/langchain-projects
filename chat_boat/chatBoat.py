96ab2fbb2ca19b3994f037653017da9382fc2401
Muzammil <muzammil844641@gmail.com>
2026-03-11 23:47:11 +0530

    feat(ChatBoat) ChatBoat is compete here wit text generetion and faster then chatgpt :

--- chat_boat/chatBoat.py
+++ chat_boat/chatBoat.py
@@ -2,35 +2,80 @@ from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 import os
 import streamlit as st
+from langchain.agents import create_agent
+from langchain_core.tools import tool
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from langgraph.checkpoint.memory import MemorySaver
+
 load_dotenv()
 
 llm = os.getenv("GROQ_API")
+serper = os.getenv("SERPER_API")
+model = ChatGroq(model="llama-3.1-8b-instant",api_key=llm , streaming=True)
+search = GoogleSerperAPIWrapper( serper_api_key=serper)
 
-model = ChatGroq(api_key=llm,model="llama-3.1-8b-instant")
+
+tools = [search.run]
+
+
+
+memory = MemorySaver()
+
+if "memory" is not st.session_state:
+    st.session_state.memory = MemorySaver()
 
 
 
 
-st.title("Jarwis AI ChatBoat")
-st.markdown("this an ai chatboat that help you ask anything whatever you want ")
+agetn1 = create_agent(
+    model=model ,
+    tools=tools,
+    system_prompt="you are smart ai chat boat that first answering from the llm data and if information is not in lmm then call tool ok ",
+    checkpointer=st.session_state.memory
 
-if "messages" not in  st.session_state:
-  st.session_state.messages = []
 
-history = []
+                    )
 
-for data in st.session_state.messages:
-    role = data["role"]
-    content = data["content"]
 
-    st.chat_message(role).markdown(content)
 
-input_data = st.chat_input("ask any question")
 
-if input_data:
-  st.session_state.messages.append({"role":"user","content":input_data})
-  st.chat_message("user").markdown(input_data)
-  res = model.invoke( st.session_state.messages)
-  st.chat_message("ai").markdown(res.content)
-  st.session_state.messages.append({"role":"ai","content":res.content})
+st.title("AI Chat Boat")
 
+
+if "messages"  not in st.session_state:
+    st.session_state.messages = []
+
+
+
+input_chat = st.chat_input("Ask ? : ")
+
+
+for message in st.session_state.messages:
+    role = message["role"]
+    mess = message["content"]
+    st.chat_message(role).markdown(mess)
+
+
+if input_chat:
+    st.chat_message("user").markdown(input_chat)
+    st.session_state.messages.append(
+    {"role":"user","content":input_chat}
+)
+    
+    res = agetn1.stream(
+    {"messages": st.session_state.messages},
+    {"configurable":{"thread_id":"1"}},
+    stream_mode="messages"
+)
+    
+    ai_container = st.chat_message("ai")
+    with ai_container:
+         space = st.empty()
+         message = ""
+
+         for chunck in res:
+             message = message + chunck[0].content
+             space.write(message)
+
+
+         st.session_state.messages.append({"role":"ai" , "content":message})
