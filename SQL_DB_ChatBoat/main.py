9b226aeebf07a47c5d6271db5fc40f7ec38591ed
Muzammil <muzammil844641@gmail.com>
2026-03-18 23:57:46 +0530

    feat(sql chat boat agent) start with connecting sql database

---
+++ SQL_DB_ChatBoat/main.py
@@ -0,0 +1,74 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import os
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langgraph.checkpoint.memory import InMemorySaver
+from langchain.agents import create_agent
+load_dotenv()
+
+
+groq_api = os.getenv("GROQ_API")
+model = ChatGroq(api_key=groq_api , model="llama-3.3-70b-versatile")
+
+
+
+db  = SQLDatabase.from_uri("sqlite:///SQL_DB.db")
+
+
+db.run("""
+    CREATE TABLE IF NOT EXISTS todo(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT,
+    status TEXT CHECK( status IN ('pending' , 'work_in_progress','completed')) DEFAULT 'pending',
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    """
+)
+
+
+toolkit = SQLDatabaseToolkit(db=db , llm=model)
+tools = toolkit.get_tools()
+memory = InMemorySaver()
+system_prompt = """
+
+You are a task management assistant that interacts with a SQL database containing a 'todo' table. 
+
+TASK RULES:
+1. Limit SELECT queries to 10 results max with ORDER BY create_at DESC
+2. After CREATE/UPDATE/DELETE, confirm with SELECT query
+3. If the user requests a list of todo, present the output in a structured table format to ensure a clean and organized display in the browser."
+
+CRUD OPERATIONS:
+    CREATE: INSERT INTO todo(title, description, status)
+    READ: SELECT * FROM todo WHERE ... LIMIT 10
+    UPDATE: UPDATE todo SET status=? WHERE id=? OR title=?
+    DELETE: DELETE FROM todo WHERE id=? OR title=?
+
+Table schema: id, title, description, status(pending/work_in_progress/completed), create_at.
+"""
+
+
+# agent code
+ 
+
+agent = create_agent(
+  model=model,
+  tools=tools,
+  system_prompt=system_prompt,
+  checkpointer=memory
+
+ )
+
+
+
+while True :
+    user_input = input("User :")
+    res = agent.invoke(
+        {"messages":[{"role":"user","content":user_input}]},
+        {"configurable":{"thread_id":"1"}}
+    )
+
+
+    print("AI:"+ res["messages"][-1].content)
